1c2172a8dbdb52bdf315f74529144e0b4694a29f
wanabe <s.wanabe@gmail.com>
2021-12-05 22:57:17 +0900

    chore: fetch x11docker if it doesn't exist

--- dev.sh
+++ dev.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+which x11docker > /dev/null || {
+    curl https://raw.githubusercontent.com/mviereck/x11docker/master/x11docker > $HOME/.local/bin/x11docker
+    chmod a+x $HOME/.local/bin/x11docker
+}
 docker build -t cmod:dev docker/
 [ -z "$1" ] && set -- yarn dev
 x11docker -m --share=$(pwd) --workdir=$(pwd) --sudouser --gpu --cap-default -- --security-opt seccomp=unconfined -- cmod:dev "$@"
